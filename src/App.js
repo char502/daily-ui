b0f0866dec1cbbf13f6212ffdeb678c049c8b48d
char502 <char502@hotmail.com>
2019-08-02 22:35:13 +0100

    background and alignment tweaks

--- src/App.js
+++ src/App.js
@@ -11,25 +11,29 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Daily-UI Project Links</h1>
+        <h1 class="title">Daily-UI Project Links</h1>
         <Container>
           <Row>
-            <Col lg={4}>
+            <Col lg={3}>
               <ol>
                 <li>
                   <a href="https://codepen.io/char502/pen/voJgVg">
                     Sign Up Page{" "}
                   </a>{" "}
                 </li>
-                <li>Credit Card Checkout Form or Page</li>
+                <li>
+                  <a href="https://codepen.io/char502/pen/YmrWZP">
+                    Credit Card Checkout Form{" "}
+                  </a>{" "}
+                </li>
                 <li>Item 3</li>
                 <li>Item 4</li>
                 <li>Item 5</li>
                 <li>Item 6</li>
               </ol>
             </Col>
-            <Col lg={8}>
-              <p>The sidebar contains links of all my Daily-UI Projects</p>
+            <Col lg={9}>
+              <p>Links to all my Daily-UI Projects</p>
             </Col>
           </Row>
         </Container>
